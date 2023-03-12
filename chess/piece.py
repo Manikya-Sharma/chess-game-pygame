ab2f9f8fbe478e02a752aae509fef7f14ec09536
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2023-03-12 16:11:36 +0530

    added queen moves

--- chess/piece.py
+++ chess/piece.py
@@ -154,6 +154,82 @@ class Queen(ChessPiece):
     def __init__(self, color, row, column, image, face_direction=+1):
         super().__init__(color, row, column, image, face_direction)
 
+    def get_next_possible_moves(self, board):
+        possible_moves_d1 = []
+        possible_moves_d2 = []
+        stop_d1 = False
+        stop_d2 = False
+        for cell in board.get_all_squares_in_diagonal(self.row, self.column):
+            # top left to bottom right and bottom left to top right
+            if cell.has_piece() and cell.piece == self:
+                continue
+            # diagonal-1
+            if cell.row < self.row and cell.column < self.column:
+                if cell.has_piece():
+                    possible_moves_d1.clear()
+                    if cell.piece.color == self.color:
+                        continue
+                possible_moves_d1.append(cell)
+            elif cell.row > self.row and cell.column > self.column:
+                if stop_d1:
+                    continue
+                if cell.has_piece():
+                    stop_d1 = True
+                    if cell.piece.color == self.color:
+                        continue
+                possible_moves_d1.append(cell)
+            # diagonal-2
+            elif cell.row > self.row and cell.column < self.column:
+                if cell.has_piece():
+                    possible_moves_d2.clear()
+                    if cell.piece.color == self.color:
+                        continue
+                possible_moves_d2.append(cell)
+            elif cell.row < self.row and cell.column > self.column:
+                if stop_d2:
+                    continue
+                if cell.has_piece():
+                    stop_d2 = True
+                    if cell.piece.color == self.color:
+                        continue
+                possible_moves_d2.append(cell)
+        possible_moves_d1.extend(possible_moves_d2)
+
+        possible_moves_row = []
+        possible_moves_column = []
+        for row_sq in board.get_all_squares_in_row(self.row):
+            if row_sq.has_piece() and row_sq.piece == self:
+                continue
+            if row_sq.has_piece():
+                if row_sq.column < self.column:
+                    possible_moves_row.clear()  # remove all squares to left
+                    if row_sq.piece.color != self.color:
+                        possible_moves_row.append(row_sq)
+                elif row_sq.column > self.column:
+                    if row_sq.piece.color != self.color:
+                        possible_moves_row.append(row_sq)
+                    break
+                continue
+            possible_moves_row.append(row_sq)
+        for column_sq in board.get_all_squares_in_column(self.column):
+            if column_sq.has_piece() and column_sq.piece == self:
+                continue
+            if column_sq.has_piece():
+                if column_sq.row < self.row:
+                    possible_moves_column.clear()  # remove all squares to top
+                    if column_sq.piece.color != self.color:
+                        possible_moves_column.append(column_sq)
+                elif column_sq.row > self.row:
+                    if column_sq.piece.color != self.color:
+                        possible_moves_column.append(column_sq)
+                    break
+                continue
+            possible_moves_column.append(column_sq)
+        possible_moves_row.extend(possible_moves_column)
+
+        possible_moves_row.extend(possible_moves_d1)
+        return possible_moves_row
+
 
 class King(ChessPiece):
     def __init__(self, color, row, column, image, face_direction=+1):
